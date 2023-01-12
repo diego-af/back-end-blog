3adc0a9d7c53ce96b6f42ecbb8ee8641bf59c69b
Diego Alves <dyegoo.1295@gmail.com>
2023-01-12 08:55:44 -0300

    Mudança no packjson

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-import { router } from "./src/routes";
+const router = require("./src/routes");
 
 const app = express();
 const port = process.env.PORT || 3000;
